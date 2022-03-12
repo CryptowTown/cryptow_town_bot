476521daefeb85876c5f5085720dab2e0521fdfb
Libardo <libardojesusrengifo129@gmail.com>
2022-03-12 19:48:53 -0400

    refactoring and divide variables values

--- index.js
+++ index.js
@@ -1,56 +1,21 @@
-const { client, token, ROLES, SERVER } = require("./config");
+const {
+  client,
+  token,
+  ROLES,
+  CHANNELS,
+  MESSAGE_BOT_VERIFICATION,
+  MESSAGE_BOT_VERIFICATION_EMOJI,
+} = require("./config");
 const {
   isCommand,
   getCommand,
-  getUsersByRole,
   getAllMembers,
   getUsersLengthByRole,
 } = require("./utils");
 
 client.on("ready", async () => {
-  const verifyChannel = client.channels.cache.get("946809488164413511");
-  const exampleEmbed = {
-    color: 0x0099ff,
-    title: "User Verification",
-    thumbnail: {
-      url: "https://i.imgur.com/sWcAXL6.jpg",
-    },
-    description: `✨Welcome to Cryptow Town, to verify yourself and enjoy the content of the server we invite you to react with the <:towfirehi:946865774474186802>\n
-      In favor of preserving a community with a super nice and healthy environment, we recommend you to read and follow our rules of coexistence, thank you for helping us to preserve the order and harmony of CrypTow Town.  
-
-      scroll Rules of peaceful community: 
-      
-       A community where friendship grows and bonds of friendship are created!  
-      
-       Please be respectful with everyone in our community, and if you are being assaulted in any way, please take a screenshot and go directly to a moderator, in case the moderator is not available you can upload the screenshot to the channel **『🚨』report-user**, we will always be attentive to your needs.
-      
-       Avoid being an author or accomplice of bullying, disrespect, toxicity, racism, misogyny, etc. If you see that any member of the community is violating this rule against you or someone else, send a direct message to a mod or report it to **『🚨』report-user**, we will review the case and do our best to resolve any misunderstanding in the public chat, and fix the situation in the best possible way.
-      `,
-    image: {
-      url: "https://i.imgur.com/sWcAXL6.jpg",
-    },
-  };
-
-  // client.channels.fetch("946809488164413511").then((channel) => {
-  //   channel.messages.fetch("952324565806235678").then((message) => {
-  //     message.react("<:towfirehi:946865774474186802>");
-  //   });
-  // });
-  //verifyChannel.send({ embeds: [exampleEmbed] });
-
   console.log(`${client.user.username} esta preparado!`);
   client.user.setActivity("con mis amigos 😎.");
-
-  const server = client.guilds.cache.get(SERVER.ID);
-  // await server.members.fetch();
-  // const roles = server.roles.cache.get(ROLES.TOW_LOYAL_ROLE_ID).members;
-
-  const allRoles = await server.roles.fetch();
-  console.log(allRoles.map((role) => ({ name: role.name, id: role.id })));
-  // const allRolesName = allRoles.map((role) => ({
-  //   name: role.name,
-  //   members: role.members,
-  // }));
 });
 
 client.on("messageCreate", async (message) => {
@@ -112,9 +77,9 @@ client.on("messageReactionAdd", async (reaction, user) => {
   console.log(user.username);
 
   if (
-    reaction.message.channelId === "946809488164413511" &&
-    reaction.message.id === "952324565806235678" &&
-    reaction.emoji.name === "towfirehi"
+    reaction.message.channelId === CHANNELS.VERIFICATION_CHANNEL &&
+    reaction.message.id === MESSAGE_BOT_VERIFICATION &&
+    reaction.emoji.name === MESSAGE_BOT_VERIFICATION_EMOJI
   ) {
     const member = await reaction.message.guild.members.fetch(user.id);
     const townRoyalUsersLength = await getUsersLengthByRole(
